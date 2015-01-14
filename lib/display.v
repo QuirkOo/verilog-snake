715e9aeebf64a33e5a0ff88a65244c5cbd91c30a
QuirkOo <beata@obrok.eu>
2015-01-15 00:54:56 +0100

    Moved all MATRIX assignments into case

--- lib/display.v
+++ lib/display.v
@@ -1,47 +1,49 @@
 module display (
 	input clk,
-	input [7:0] pos,
-	input [7:0] tailPos,
+	input [8*16-1:0] pixelRow,
 	output reg [7:0] MATRIX_ROW,
 	output reg [15:0] MATRIX_COL
 );
 
 reg [2:0] i = 0;
 
-reg [8*15:0] pixelRow;
-reg [15:0] temp;
-
-reg [3:0] x, y;
-
 always @(posedge clk) begin
 	case (i)
-		3'b001: MATRIX_ROW <= 8'b11111110;
-		3'b010: MATRIX_ROW <= 8'b11111101;
-		3'b011: MATRIX_ROW <= 8'b11111011;
-		3'b100: MATRIX_ROW <= 8'b11110111;
-		3'b101: MATRIX_ROW <= 8'b11101111;
-		3'b110: MATRIX_ROW <= 8'b11011111;
-		3'b111: MATRIX_ROW <= 8'b10111111;
-		3'b000: MATRIX_ROW <= 8'b01111111;
+		3'b001: begin
+			MATRIX_ROW <= 8'b11111110;
+			MATRIX_COL[15:0] <= pixelRow[15:0];
+		end
+		3'b010: begin
+			MATRIX_ROW <= 8'b11111101;
+			MATRIX_COL[15:0] <= pixelRow[31:16];
+		end
+		3'b011: begin
+			MATRIX_ROW <= 8'b11111011;
+			MATRIX_COL[15:0] <= pixelRow[47:32];
+		end
+		3'b100: begin
+			MATRIX_ROW <= 8'b11110111;
+			MATRIX_COL[15:0] <= pixelRow[63:48];
+		end
+		3'b101: begin
+			MATRIX_ROW <= 8'b11101111;
+			MATRIX_COL[15:0] <= pixelRow[79:64];
+		end
+		3'b110: begin
+			MATRIX_ROW <= 8'b11011111;
+			MATRIX_COL[15:0] <= pixelRow[95:80];
+		end
+		3'b111: begin
+			MATRIX_ROW <= 8'b10111111;
+			MATRIX_COL[15:0] <= pixelRow[111:96];
+		end
+		3'b000: begin
+			MATRIX_ROW <= 8'b01111111;
+			MATRIX_COL[15:0] <= pixelRow[127:112];
+		end
 	endcase
-		
-	MATRIX_COL[15:0] <= pixelRow[15:0];
 	i = i + 1;
 	
-end
-
-always @(tailPos or pos) begin
-	x[3:0] <= tailPos[7:4];
-	y[3:0] <= tailPos[3:0];
-	temp = pixelRow[x];
-	temp[y] = 1;
-	pixelRow[x] = temp;
-	
-	x[3:0] <= pos[7:4];
-	y[3:0] <= pos[3:0];
-	temp = pixelRow[x];
-	temp[y] = 0;
-	pixelRow[x] = temp;
 end
 
 endmodule
