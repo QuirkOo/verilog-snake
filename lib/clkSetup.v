9056c8e4aaa1e79d87b20b89f75ce502430c6291
QuirkOo <beata@obrok.eu>
2015-01-16 23:44:34 +0100

    Add module for clock setup

---
+++ lib/clkSetup.v
@@ -0,0 +1,17 @@
+module clkSetup (
+	input clk,
+	output displayClk,
+	output updateClk
+);
+
+prescaler #(2) prescaler1 (
+	.clkIn(clk),
+	.clkOut(updateClk)
+);
+
+prescaler #(1024) prescaler2 (
+	.clkIn(clk),
+	.clkOut(displayClk)
+);
+
+endmodule
